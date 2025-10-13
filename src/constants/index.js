27e391fea23101139682ecdd4947f425eea5a5bb
american beloved <americanbeloved254@gmail.com>
2025-10-13 23:13:00 +0300

    initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const navlinks = [
+    {label: 'store'},
+    {label: 'mac'},
+    {label: 'ipad'},
+    {label: 'iphone'},
+    {label: 'watch'},
+    {label: 'airpods'},
+    {label: 'vision'},
+    {label: 'only on apple'},
+    {label: 'accessories'},
+    {label: 'support'},
+]
